603fa210c9ce67beda2f5519b15b0de7f5dc226c
regina130902 <reginanurpaidah@gmail.com>
2022-12-12 14:02:38 +0700

    data pasien

---
+++ database/migrations/2022_12_12_064501_create_perawats_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePerawatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perawats', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->date('tanggal_lahir');
+            $table->string('alamat');
+            $table->string('jenis_kelamin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perawats');
+    }
+}
